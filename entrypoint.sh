ed7e7a66638a57c1bac26baac66d6e22e630d6ac
Alessandro Berlati <alessandro.berlati@gmail.com>
2020-06-05 18:22:56 +0200

    fix: use analytics in api entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -9,7 +9,7 @@ API_WORKER_MAX_REQUESTS=${API_WORKER_MAX_REQUESTS:-10000}
 CELERY_WORKER_CONCURRENCY=${CELERY_WORKER_CONCURRENCY:-2}
 
 case "$1" in
-    api) poetry run gunicorn immuni_exposure_ingestion.sanic:sanic_app \
+    api) poetry run gunicorn immuni_analytics.sanic:sanic_app \
             --access-logfile='-' \
             --bind=${API_HOST}:${API_PORT} \
             --max-requests=${API_WORKER_MAX_REQUESTS} \
