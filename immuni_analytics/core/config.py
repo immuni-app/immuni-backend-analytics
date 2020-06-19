25d5eed4d9021abf9f287137731d2a58c96b685b
Omar Cardia <oc@bendingspoons.com>
2020-06-19 17:18:26 +0200

    refactor(config): sort variables alphabetically

--- immuni_analytics/core/config.py
+++ immuni_analytics/core/config.py
@@ -11,8 +11,6 @@
 #  You should have received a copy of the GNU Affero General Public License
 #  along with this program. If not, see <https://www.gnu.org/licenses/>.
 
-import logging
-
 from decouple import config
 
 from immuni_analytics.models.enums import CeleryAppName
@@ -20,35 +18,12 @@ from immuni_common.core.config import ENV
 from immuni_common.helpers.config import load_certificate, validate_crontab
 from immuni_common.models.enums import Environment
 
-_LOGGER = logging.getLogger(__name__)
-
-
-ANALYTICS_MONGO_URL = config(
-    "ANALYTICS_MONGO_URL", default="mongodb://localhost:27017/immuni-analytics-dev"
-)
 ANALYTICS_BROKER_REDIS_URL: str = config(
     "ANALYTICS_BROKER_REDIS_URL", default="redis://localhost:6379/1"
 )
-EXPOSURE_PAYLOAD_QUEUE_KEY: str = config(
-    "EXPOSURE_PAYLOAD_QUEUE_KEY", default="ingested_exposure_data"
+ANALYTICS_MONGO_URL = config(
+    "ANALYTICS_MONGO_URL", default="mongodb://localhost:27017/immuni-analytics-dev"
 )
-EXPOSURE_PAYLOAD_ERRORS_QUEUE_KEY: str = config(
-    "EXPOSURE_PAYLOAD_ERRORS_QUEUE_KEY", default="errors_exposure_data"
-)
-OPERATIONAL_INFO_QUEUE_KEY: str = config("OPERATIONAL_INFO_QUEUE_KEY", default="operational_info")
-CELERY_ALWAYS_EAGER: bool = config(
-    "CELERY_ALWAYS_EAGER", cast=bool, default=ENV == Environment.TESTING
-)
-CELERY_BROKER_REDIS_URL_AUTHORIZATION: str = config(
-    "CELERY_BROKER_REDIS_URL_AUTHORIZATION", default="redis://localhost:6379/0"
-)
-CELERY_BROKER_REDIS_URL_SCHEDULED: str = config(
-    "CELERY_BROKER_REDIS_URL_SCHEDULED", default="redis://localhost:6379/0"
-)
-CELERY_APP_NAME: CeleryAppName = config(
-    "CELERY_APP_NAME", cast=CeleryAppName.from_env_var, default=CeleryAppName.AUTHORIZATION
-)
-
 ANALYTICS_TOKEN_SIZE: int = config("ANALYTICS_TOKEN_SIZE", cast=int, default=128)
 APPLE_CERTIFICATE_KEY: str = config(
     "APPLE_CERTIFICATE_KEY", cast=load_certificate("APPLE_CERTIFICATE_KEY"), default=""
@@ -61,6 +36,19 @@ APPLE_DEVICE_CHECK_URL: str = config(
 )
 APPLE_KEY_ID: str = config("APPLE_KEY_ID", default="")
 APPLE_TEAM_ID: str = config("APPLE_TEAM_ID", default="")
+
+CELERY_ALWAYS_EAGER: bool = config(
+    "CELERY_ALWAYS_EAGER", cast=bool, default=ENV == Environment.TESTING
+)
+CELERY_APP_NAME: CeleryAppName = config(
+    "CELERY_APP_NAME", cast=CeleryAppName.from_env_var, default=CeleryAppName.AUTHORIZATION
+)
+CELERY_BROKER_REDIS_URL_AUTHORIZATION: str = config(
+    "CELERY_BROKER_REDIS_URL_AUTHORIZATION", default="redis://localhost:6379/0"
+)
+CELERY_BROKER_REDIS_URL_SCHEDULED: str = config(
+    "CELERY_BROKER_REDIS_URL_SCHEDULED", default="redis://localhost:6379/0"
+)
 CHECK_TIME_SECONDS_MAX: int = config("CHECK_TIME_SECONDS_MAX", cast=int, default=10)
 CHECK_TIME_SECONDS_MIN: int = config("CHECK_TIME_SECONDS_MIN", cast=int, default=7)
 DATA_RETENTION_DAYS: int = config("DATA_RETENTION_DAYS", cast=int, default=30)
@@ -70,16 +58,36 @@ DELETE_OLD_DATA_PERIODICITY: str = config(
     cast=validate_crontab("DELETE_OLD_DATA_PERIODICITY"),
     default="0 0 * * *",
 )
+
+EXPOSURE_PAYLOAD_ERRORS_QUEUE_KEY: str = config(
+    "EXPOSURE_PAYLOAD_ERRORS_QUEUE_KEY", default="errors_exposure_data"
+)
 EXPOSURE_PAYLOAD_MAX_INGESTED_ELEMENTS: int = config(
     "EXPOSURE_PAYLOAD_MAX_INGESTED_ELEMENTS", cast=int, default=100
 )
+EXPOSURE_PAYLOAD_QUEUE_KEY: str = config(
+    "EXPOSURE_PAYLOAD_QUEUE_KEY", default="ingested_exposure_data"
+)
+
+OPERATIONAL_INFO_QUEUE_KEY: str = config("OPERATIONAL_INFO_QUEUE_KEY", default="operational_info")
 OPERATIONAL_INFO_MAX_INGESTED_ELEMENTS: int = config(
     "OPERATIONAL_INFO_MAX_INGESTED_ELEMENTS", cast=int, default=100
 )
+
 READ_TIME_SECONDS_MAX: int = config("READ_TIME_SECONDS_MAX", cast=int, default=3)
 READ_TIME_SECONDS_MIN: int = config("READ_TIME_SECONDS_MIN", cast=int, default=0)
 REQUESTS_TIMEOUT_SECONDS: int = config("REQUESTS_TIMEOUT_SECONDS", cast=int, default=5)
+
+SAFETY_NET_APK_DIGEST: str = config("SAFETY_NET_APK_DIGEST", default="")
+SAFETY_NET_ISSUER_HOSTNAME: str = config("SAFETY_NET_ISSUER_HOSTNAME", default="attest.android.com")
 SAFETY_NET_MAX_SKEW_MINUTES: int = config("SAFETY_NET_MAX_SKEW_MINUTES", cast=int, default=10)
+SAFETY_NET_PACKAGE_NAME: str = config(
+    "SAFETY_NET_PACKAGE_NAME", default="it.ministerodellasalute.immuni"
+)
+SALT_LENGTH: int = config("SALT_LENGTH", cast=int, default=24)
+SIGNED_ATTESTATION_MAX_LENGTH: int = config(
+    "SIGNED_ATTESTATION_MAX_LENGTH", cast=int, default=10_000
+)
 STORE_INGESTED_DATA_PERIODICITY: str = config(
     "STORE_INGESTED_DATA_PERIODICITY",
     cast=validate_crontab("STORE_INGESTED_DATA_PERIODICITY"),
@@ -90,15 +98,3 @@ STORE_OPERATIONAL_INFO_PERIODICITY: str = config(
     cast=validate_crontab("STORE_OPERATIONAL_INFO_PERIODICITY"),
     default="* * * * *",
 )
-SAFETY_NET_APK_DIGEST: str = config("SAFETY_NET_APK_DIGEST", default="")
-SAFETY_NET_ISSUER_HOSTNAME: str = config("SAFETY_NET_ISSUER_HOSTNAME", default="attest.android.com")
-SAFETY_NET_PACKAGE_NAME: str = config(
-    "SAFETY_NET_PACKAGE_NAME", default="it.ministerodellasalute.immuni"
-)
-SALT_LENGTH: int = config("SALT_LENGTH", cast=int, default=24)
-SIGNED_ATTESTATION_MAX_LENGTH: int = config(
-    "SIGNED_ATTESTATION_MAX_LENGTH", cast=int, default=10_000
-)
-
-DUMMY_REQUEST_TIMEOUT_MILLIS: int = config("DUMMY_REQUEST_TIMEOUT_MILLIS", cast=int, default=150)
-DUMMY_REQUEST_TIMEOUT_SIGMA: int = config("DUMMY_REQUEST_TIMEOUT_SIGMA", cast=int, default=20)
