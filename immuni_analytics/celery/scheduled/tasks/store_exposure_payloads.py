d138373374e4ebed7c86022e26904d9193366880
Alfonso D'amelio <31849245+alfo1995@users.noreply.github.com>
2021-01-15 09:21:57 +0100

    fix: invalidate_cun (#48)
    * fix: removed request to his service in the store_exposure_payloads task
    * style: run checks

--- immuni_analytics/celery/scheduled/tasks/store_exposure_payloads.py
+++ immuni_analytics/celery/scheduled/tasks/store_exposure_payloads.py
@@ -22,7 +22,6 @@ from mongoengine import ValidationError as MongoengineValidationError
 from immuni_analytics.celery.scheduled.app import celery_app
 from immuni_analytics.core import config
 from immuni_analytics.core.managers import managers
-from immuni_analytics.helpers.his_external_service import invalidate_cun
 from immuni_analytics.models.exposure_data import ExposurePayload
 from immuni_analytics.monitoring.celery import STORED_EXPOSURE_PAYLOAD, WRONG_EXPOSURE_PAYLOAD
 from immuni_common.core.exceptions import ImmuniException
@@ -56,7 +55,6 @@ async def _store_exposure_payloads() -> None:
     pipe.ltrim(config.EXPOSURE_PAYLOAD_QUEUE_KEY, config.EXPOSURE_PAYLOAD_MAX_INGESTED_ELEMENTS, -1)
     ingested_data = (await pipe.execute())[0]
 
-    cun_to_invalidate = []
     bad_format_data = []
     exposure_data = []
 
@@ -64,7 +62,6 @@ async def _store_exposure_payloads() -> None:
         try:
             json_decoded = json.loads(element)
             exposure_payload = _load_exposure_payload(json_decoded)
-            cun_to_invalidate.append(_load_cun_attributes(json_decoded))
         except (
             MongoengineValidationError,
             MarshmallowValidationError,
@@ -86,17 +83,6 @@ async def _store_exposure_payloads() -> None:
             "Found ingested data with bad format.", extra={"bad_format_data": n_bad_format_data}
         )
         WRONG_EXPOSURE_PAYLOAD.inc(n_bad_format_data)
-
-    # Request to invalidate CUN through HIS service
-    for cun in cun_to_invalidate:
-        id_test_verification = cun.get("id_test_verification", None)
-        token_sha = cun.get("token_sha", None)
-        if id_test_verification and token_sha:
-            invalidate_cun(cun_sha=token_sha, id_test_verification=id_test_verification)
-            _LOGGER.info(
-                "Calling HIS service to invalidate CUN.",
-                extra={"cun_sha": token_sha, "id_test_verification": id_test_verification},
-            )
 
     queue_length = await managers.analytics_redis.llen(config.EXPOSURE_PAYLOAD_QUEUE_KEY)
     _LOGGER.info(
@@ -122,22 +108,3 @@ def _load_exposure_payload(json_decoded: dict) -> ExposurePayload:
     exposure_payload.validate()
 
     return exposure_payload
-
-
-def _load_cun_attributes(json_decoded: dict) -> dict:
-    """
-    Extract from a dictionary two attributes and add to to a new dictionary.
-
-    :param json_decoded: the dictionary from which to extract attributes.
-    :return: a dict with two keys.
-    :raises: InvalidFormatException.
-    """
-    if not (
-        json_decoded.get("version", None) == 1 and (payload := json_decoded.get("payload", None))
-    ):
-        raise InvalidFormatException()
-
-    id_test_verification = payload.get("id_test_verification", None)
-    token_sha = payload.get("token_sha", None)
-
-    return dict(id_test_verification=id_test_verification, token_sha=token_sha)
